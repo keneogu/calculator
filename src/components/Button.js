336547ebb5b5240841583b12005c1aef11339f2f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-29 16:12:12 +0100

    Add button component that accepts a button name as props

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const Button = ({name}) => {
+	return (
+		<button>{name}</button>
+	)
+}
+
+Button.propTypes = {
+	name: PropTypes.string.isRequired
+}
+
+export default Button;
