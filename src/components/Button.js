5b6ff6f814b0b1f711133260859525cb45a9cbb4
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-29 21:30:29 +0100

    Add buttonPanel components that renders the calculator panel with all the buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 
-const Button = ({name}) => {
-	return (
-		<button>{name}</button>
-	)
-}
+const Button = ({ name }) => (
+  <button type="button">{name}</button>
+);
 
 Button.propTypes = {
-	name: PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Display = ({result}) => {
-	return (
-		<div>
-			<h1>{result}</h1>
-		</div>
-	);
-}
+const Display = ({ result }) => (
+  <div>
+    <h1>{result}</h1>
+  </div>
+);
 
 Display.defaultProps = {
-	result: '0',
+  result: '0',
 };
 
 Display.propTypes = {
-	result: PropTypes.string
+  result: PropTypes.string,
 };
 
 export default Display;
