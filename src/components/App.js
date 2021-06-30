b0edcfb4a6c37427afc32c69386ba0ecc3102a72
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-30 13:53:05 +0100

    Merge branch 'development' into milestone-two

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 function App() {
+  calculate();
   return (
     <>
       <Display />
