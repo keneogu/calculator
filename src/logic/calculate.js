f8b1e2da6da84db4ffdbc5d926df1826b4f7610b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-30 13:54:19 +0100

    Modified the calculate.js file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,19 +1,19 @@
 import operate from './operate';
 
 function calculate(calculator, buttonName) {
-  let {total,next,operation} = calculator;
-  
+  let { total, next, operation } = calculator;
+
   if (buttonName === '+/-') {
-	total *= -1;
-	next *= -1;
-  }else if (button === '+' || button === '-' || button === 'X' || button === '%' || button === '÷') {
-	total = operate(total,next,operation);
-  }else if (buttonName === 'AC') {
+    total *= -1;
+    next *= -1;
+  } else if (button === '+' || button === '-' || button === 'X' || button === '%' || button === '÷') {
+    total = operate(total, next, operation);
+  } else if (buttonName === 'AC') {
     total = '0';
     next = '';
     operation = '';
   }
-  return {total,next,operation};
+  return { total, next, operation };
 }
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,34 +1,34 @@
 import Big from 'big.js';
 
-function operate(numberOne,numberTwo,operation) {
+function operate(numberOne, numberTwo, operation) {
   let result;
   const num1 = Big(numberOne);
   const num2 = Big(numberTwo);
 
-  switch(operation) {
-	case '+':
-	  result = num1.plus(num2);
-	  break;
-	case '-':
-	  result = num1.minus(num2);
-	  break;
-	case 'X':
-	  result = num1.times(num2);
-	  break;
-	case '÷':
-	  result = num1.div(num2);
-	  break;
-	case '%':
-	  result = (num1.times(num2)).div(100);
-	  break;
-	case '+/-':
-	  result = num1.times(-1);
-	  break;
-	default:
-	  result = 0;
+  switch (operation) {
+    case '+':
+      result = num1.plus(num2);
+      break;
+    case '-':
+      result = num1.minus(num2);
+      break;
+    case 'X':
+      result = num1.times(num2);
+      break;
+    case '÷':
+      result = num1.div(num2);
+      break;
+    case '%':
+      result = num1.times(num2).div(100);
+      break;
+    case '+/-':
+      result = num1.times(-1);
+      break;
+    default:
+      result = 0;
   }
 
   return result.toString();
-};
+}
 
 export default operate;
