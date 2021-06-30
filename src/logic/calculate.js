3357313a5afde7f9b59a39151c6f47114cef1a5f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-30 12:14:37 +0100

    Implement calculate.js module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,19 @@
-import Big from 'big.js';
+import operate from './operate';
 
 function calculate(calculator, buttonName) {
+  let {total,next,operation} = calculator;
   
+  if (buttonName === '+/-') {
+	total *= -1;
+	next *= -1;
+  }else if (button === '+' || button === '-' || button === 'X' || button === '%' || button === '÷') {
+	total = operate(total,next,operation);
+  }else if (buttonName === 'AC') {
+    total = '0';
+    next = '';
+    operation = '';
+  }
+  return {total,next,operation};
 }
+
+export default calculate;
