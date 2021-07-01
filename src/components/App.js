bc108d7d714f37d7a65b53f7b115f805e3c49b60
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-01 14:14:27 +0100

    Create a stateful parent component with three properties

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,24 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display calculate={calculate} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      total: '',
+      next: '',
+      operation: null,
+    }
+  }
 }
+
+// function App() {
+//   return (
+//     <>
+//       <Display calculate={calculate} />
+//       <ButtonPanel />
+//     </>
+//   );
+// }
 
 export default App;
