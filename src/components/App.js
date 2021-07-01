47412d7e3f9791a8c01cbd934fddb4e1893a4768
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-01 23:29:13 +0100

    Implement handleClick function and rnder function7

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,33 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       total: '',
       next: '',
       operation: null,
-    }
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    // const { total, next, operation } = calculate(this.state, buttonName);
+    // this.setState({ total, next, operation });
+    const dataset = this.state;
+    const calc = calculate(dataset, buttonName);
+    this.setState(() => ({ ...calc }));
+  }
+
+  render() {
+   
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
   }
 }
-
-// function App() {
-//   return (
-//     <>
-//       <Display calculate={calculate} />
-//       <ButtonPanel />
-//     </>
-//   );
-// }
 
 export default App;
