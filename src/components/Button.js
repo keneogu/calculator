4e8c46003102d2d553ec18c7c14211b6f069c2fb
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-02 16:22:30 +0100

    Add in-style for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">{name}</button>
+const Button = ({ name, handleClick }) => (
+  <button type="button" style={{ height: '30px', width: '50px' }} onClick={() => handleClick(name)}>{name}</button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Button;
