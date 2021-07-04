22dd06e3a50656af12b112ef89920391266f6635
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-04 14:30:10 +0100

    Refactored App.js using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: '',
-      operation: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [data, setData] = useState({ total: '0', next: '', operation: '' });
 
-  handleClick = (buttonName) => {
-    const { total, next, operation } = calculate(this.state, buttonName);
-    this.setState({ total, next, operation });
-  }
+  const handleClick = (buttonName) => {
+    setData(calculate(data, buttonName));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    const output = operation === '=' ? total : `${total} ${operation} ${next}`;
-    return (
-      <>
-        <Display result={output} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  const { total, next, operation } = data;
+  const output = operation === '=' ? total : `${total} ${operation} ${next}`;
+  return (
+    <>
+      <Display result={output} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
