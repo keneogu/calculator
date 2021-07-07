96d46e194c3f9223e7c3cbc745d45dd9d9126e7d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-07 15:32:42 +0100

    Add calculator page

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+const Calculator = () => (
+  <div className="cal">
+    <h2>Compute your problems here.</h2>
+    <div>
+      <App />
+    </div>
+  </div>
+);
+
+export default Calculator;
