e5f8592b9b59d01c6e23ae68971b735cea06d732
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-10 10:52:36 +0100

    Add snapshot test for home page

--- src/components/__test__/calculate.test.js
+++ src/components/__test__/calculate.test.js
@@ -1,27 +1,27 @@
 import calculate from '../../logic/calculate';
 
 describe('calculate', () => {
-	test('clears the display when AC button is pressed', () => {
-		const data = {
-			total: '0',
-			next: '',
-			operation: '',
-		  };
-	  
-		  const newData = calculate(data, 'AC');
-		  expect(newData).toEqual(data);
-	});
-	test('display the multiplication operation when clicked', () => {
-		const myCalculate = { total: 15, operation: 'x', next: '' };
-		const data = calculate(myCalculate, 'x');
-		expect(data.operation).toEqual('x');
-	});
-	test('should calculate output when "=" pressed', () => {
-		const data = { total: 7, next: 5, operation: 'x' };
-		const newData = calculate(data, '=');
-	
-		expect(newData.total).toEqual('35');
-		expect(newData.next).toEqual('');
-		expect(newData.operation).toEqual('=');
-	});
+  test('clears the display when AC button is pressed', () => {
+    const data = {
+      total: '0',
+      next: '',
+      operation: '',
+    };
+
+    const newData = calculate(data, 'AC');
+    expect(newData).toEqual(data);
+  });
+  test('display the multiplication operation when clicked', () => {
+    const myCalculate = { total: 15, operation: 'x', next: '' };
+    const data = calculate(myCalculate, 'x');
+    expect(data.operation).toEqual('x');
+  });
+  test('should calculate output when "=" pressed', () => {
+    const data = { total: 7, next: 5, operation: 'x' };
+    const newData = calculate(data, '=');
+
+    expect(newData.total).toEqual('35');
+    expect(newData.next).toEqual('');
+    expect(newData.operation).toEqual('=');
+  });
 });
--- src/components/__test__/operate.test.js
+++ src/components/__test__/operate.test.js
@@ -1,14 +1,14 @@
 import operate from '../../logic/operate';
 
 test('should add,divide,multiply,subtract and percentage of two numbers', () => {
-	const add = operate('5', '6', '+');
-	const minus = operate('10', '3', '-');
-	const times = operate('5', '10', 'x');
-	const divide = operate('5', '10', '÷');
-	const percent = operate('100', '10', '%');
-	expect(add).toEqual('11');
-	expect(minus).toEqual('7');
-	expect(times).toEqual('50');
-	expect(divide).toEqual('0.5');
-	expect(percent).toEqual('10');
+  const add = operate('5', '6', '+');
+  const minus = operate('10', '3', '-');
+  const times = operate('5', '10', 'x');
+  const divide = operate('5', '10', '÷');
+  const percent = operate('100', '10', '%');
+  expect(add).toEqual('11');
+  expect(minus).toEqual('7');
+  expect(times).toEqual('50');
+  expect(divide).toEqual('0.5');
+  expect(percent).toEqual('10');
 });
