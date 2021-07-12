c78c28290874b35a1e3a722b884e8b28257cdcbb
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-12 10:07:52 +0100

    Fix button test fails

--- src/components/__test__/Button.test.js
+++ src/components/__test__/Button.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Button from '../Button';
 
 test('should render the button component', () => {
-  render(<Button name="4" />);
+  render(<Button name="4" wide={true} color='green' handleClick={fireEvent.click} />);
   const btn = screen.getByTestId('button');
   expect(btn.textContent).toBe('4');
 });
